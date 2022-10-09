6fe9efadc68588054ba7cf1f31bed09ad0108ee3
saurav <sauravbhr712@gmail.com>
2022-10-09 08:53:26 -0600

    simple array,a very big sum and diagonal difference solution

---
+++ solution/Diagonal Difference.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    l = 0
+    r = 0
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if (i == j):
+                l += arr[i][j]
+
+            if (j == (len(arr) - i - 1)):
+                r += arr[i][len(arr) - i - 1]
+    return abs(l - r)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
