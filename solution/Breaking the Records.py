9de7b2d93ca023f476884d7f7b3b211a368eef0d
RptR <sauravbhr712@gmail.com>
2022-12-09 06:02:27 +0530

    Breaking the record

---
+++ solution/Breaking the Records.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the breakingRecords function below.
+def breakingRecords(scores):
+    max = min = scores[0]
+    rec = [0, 0]
+    for i in range(1, len(scores)):
+        if max < scores[i]:
+            max = scores[i]
+            rec[0] = rec[0] + 1
+        if min > scores[i]:
+            min=scores[i]
+            rec[1]=rec[1] + 1
+    return rec
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    result = breakingRecords(scores)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
